df49b4fa7d998bcb6e2e87e240774240e84650d6
Daryl Bart <darylbart@gmail.com>
2023-10-03 10:21:26 -0400

    add random to questions

--- questions_loader.py
+++ questions_loader.py
@@ -1,8 +1,9 @@
-# questions_loader.py
-
 import json
+import random
 
 def load_questions(filename):
     with open(filename, 'r') as file:
         questions = json.load(file)
+    
+    random.shuffle(questions)
     return questions
